06c3a8b41f6daede162b92b85735ab200ee202c8
Osintexec <96475852+Osintexec@users.noreply.github.com>
2021-12-21 14:47:56 +0100

    Create install_base.sh
    Update Ubuntu 20.04 LTS and install Ansible

---
+++ install_base.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+## tested on Ubuntu 20.04 LTS ref. https://www.linuxtechi.com/how-to-install-ansible-on-ubuntu/
+## Apply latest update
+sudo apt update
+sudo apt updgrade -y
+## Install dependencies 
+sudo apt install -y software-properties-common
+## Configure PPA repository for Ansible 
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+## Update PPA repository
+sudo apt update
+## Install Ansible
+sudo apt install -y ansible
+####
+## Check ansible --version
+####
+
+exit 0
